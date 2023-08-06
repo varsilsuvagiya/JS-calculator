0c5af490e4c077a02a9b9f379b1b979315a37861
varsil suvagiya <varsilsuvagiya@gmail.com>
2023-08-06 20:07:29 +0530

    6-8-23

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //
 let display = document.getElementById('display');
 let buttons = Array.from(document.getElementsByClassName('button'));
+
 buttons.map(button => {
   button.addEventListener('click', (e) => {
     switch (e.target.innerText) {
